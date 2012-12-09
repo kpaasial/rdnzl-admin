4a71191d298c2e6d04a0aee11ba58212b88bbaa9
Kimmo Paasiala <kpaasial@gmail.com>
2012-12-09 15:11:23 +0200

    Script for building packages with ports-mgmt/poudriere(-devel

---
+++ build-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PORTSTXT=$1
+
+: ${PORTSTXT:="$HOME/db/ports.txt"}
+: ${SU_CMD:="/usr/bin/su root -c"}
+
+echo "Using ${PORTSTXT} as the list for ports to build."
+
+${SU_CMD} "/usr/local/bin/poudriere bulk -f ${PORTSTXT} -j release91amd64"
+
+
