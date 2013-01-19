80be983da5ad8403e9cf1d7c6cc7339c11a60519
Kimmo Paasiala <kpaasial@gmail.com>
2013-01-19 23:15:34 +0200

    Build jail is configurable now.

--- whitezone/build-packages.sh
+++ whitezone/build-packages.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 PORTSTXT=$1
+BUILD_JAIL=$2
 
 : ${PORTSTXT:="/usr/local/etc/ports.txt"}
+: ${BUILD_JAIL:="release91amd64"}
+
 
 
 echo "Using ${PORTSTXT} as the list for ports to build."
+echo "Using ${BUILD_JAIL} as the build jail."
 
-/usr/local/bin/poudriere bulk -f ${PORTSTXT} -j release91amd64
+
+/usr/local/bin/poudriere bulk -f ${PORTSTXT} -j ${BUILD_JAIL}
 
 
