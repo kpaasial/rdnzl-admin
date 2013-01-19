72bda5f7b1ef1f95297d444886eaf85899a26438
Kimmo Paasiala <kpaasial@gmail.com>
2013-01-19 13:45:06 +0200

    Simplified.

--- common/update-src.sh
+++ common/update-src.sh
@@ -5,6 +5,24 @@ SRC_DIR=/usr/src
 
 GIT=/usr/local/bin/git
 
+while getopts ciu o
+do
+    case "$o" in 
+    c)  CRONMODE=y;;  
+    esac
+
+done
+
+if [ -n "${CRONMODE}" ]; then
+    sleep `jot -r 1 0 1200`
+fi
+
 cd ${SRC_DIR} && ${GIT} fetch -v
 
+if [ -z "${CRONMODE}" ]; then
+    cd ${SRC_DIR} && ${GIT} merge -v FETCH_HEAD
+fi
+
+echo "$0 done."
+exit 0
 
