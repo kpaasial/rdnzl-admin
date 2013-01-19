de1d25fc28b29372029e53125d64f9993441e9c8
Kimmo Paasiala <kpaasial@gmail.com>
2013-01-19 02:43:15 +0200

    Script for reporting various things about installed packages. Does not need a ports tree.

---
+++ common/check-packages.sh
@@ -0,0 +1,44 @@
+#!/bin/sh --
+
+# Script for checking vulnerabilities with pkg-audit(8) and
+# other checks for installed packages.
+
+# This does not need the ports tree at /usr/ports.
+
+PKGNG=/usr/local/sbin/pkg
+
+cat <<EOT
+Updating audit database and checking for vulnerable packages.
+-------------------------------------------------------------
+EOT
+
+/usr/local/etc/periodic/security/410.pkg-audit
+
+
+cat <<EOT
+
+Checking package dependencies
+--------------------------------------
+EOT
+
+${PKGNG} check -d 
+
+
+cat <<EOT
+
+Checking package checksums
+--------------------------------------
+EOT
+
+${PKGNG} check -s 
+
+cat <<EOT
+
+Automatically added packages that are no longer depended on.
+Run '${PKGNG} autoremove' to remove them.
+--------------------------------------
+EOT
+
+${PKGNG} query -e '%a=1 && %#r=0' '%n-%v' || exit 1
+
+
