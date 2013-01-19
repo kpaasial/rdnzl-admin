60b352c1f8f857c6694f63ef661c48549f934987
Kimmo Paasiala <kpaasial@gmail.com>
2013-01-19 23:14:11 +0200

    Only fetches updates in cron mode. Merges the changes and builds a new INDEX in normal mode as well.

--- whitezone/update-ports.sh
+++ whitezone/update-ports.sh
@@ -1,101 +1,51 @@
 #!/bin/sh
 
-PKGNG=/usr/local/sbin/pkg
+# Script for updating the ports tree.
 
+PORTS_DIR=/usr/ports
+GIT=/usr/local/bin/git
 
-
-# parse options
-
-while getopts ciu o
+while getopts c o
 do
     case "$o" in 
     c)  CRONMODE=y;;  
-    i)  INITMODE=y;;
-    u)  UPDATING=y;;
     esac
 
 done
 
+# Delay for up to 1200 seconds before continuing in cron mode.
 
-
-# If run in cron mode delay for up to 1800 seconds before continuing
-
-if [ -n "$CRONMODE" ]; then
+if [ -n "${CRONMODE}" ]; then
     sleep `jot -r 1 0 1200`
 fi
 
-
-
-
-cat <<EOT
-Updating audit database and checking for vulnerable packages.
--------------------------------------------------------------
-EOT
-
-/usr/local/etc/periodic/security/410.pkg-audit
-
-
 cat <<EOT
 
-Updating /usr/ports with svn(1).
+Pulling updates for /usr/ports with git(1).
 -------------------------------------
 
 EOT
 
+cd ${PORTS_DIR} && ${GIT} fetch -v
 
-/usr/local/bin/svn up /usr/ports
 
+if [ -n "${CRONMODE}" ]; then
+    echo "$0 done."
+    exit 0
+fi 
 
-# In init mode the cache is rebuilt from scratch
-if [ -n "$INITMODE" ]; then
-    /usr/local/bin/cache-init
-else
-    cat <<EOT
+cd ${PORTS_DIR} && ${GIT} merge -v FETCH_HEAD
 
-Updating portindex-cache database.
+cat <<EOT
+
+Creating INDEX.
 -------------------------------------
 
 EOT
-    /usr/local/bin/cache-update
-fi
 
-cat <<EOT
+make -C ${PORTS_DIR} index
 
-Creating INDEX with portindex(1).
--------------------------------------
+echo "$0 done."
 
-EOT
-/usr/local/bin/portindex -o /usr/ports/INDEX-9
-
-
-
-cat <<EOT
-
-Automatically added packages that are no longer depended on.
-Run '${PKGNG} autoremove' to remove them.
---------------------------------------
-EOT
-
-${PKGNG} query -e '%a=1 && %#r=0' '%n-%v' || exit 1
-
-cat <<EOT
-
-Installed packages that have updates available:
------------------------------------------------
-
-EOT
-
-${PKGNG} version -IvL=
-
-if [ -n "$UPDATING" ]; then
-    UPD_START_DATE=$(/bin/date -v-1m +%Y%m%d)
-    cat <<EOT
-
-Warnings from /usr/ports/UPDATING for installed packages
-from ${UPD_START_DATE} to today.
----------------------------------------------------------
-EOT
-
-    ${PKGNG} updating -d ${UPD_START_DATE} || exit 1
-fi
+exit 0
 
