2f9bed1a2ac3281460a1d1b33f520cb1a484bbe1
Kimmo Paasiala <kpaasial@gmail.com>
2013-04-03 13:08:23 +0300

    .

--- firewall/update-pf-tables.sh
+++ firewall/update-pf-tables.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
 CONFIG=/usr/local/etc/pf-tables.txt
+DBDIR=/var/db/pf-tables
+
 
 while read URL TABLEFILE
 do
     if [ -n "${URL}" ]; then
         echo "URL: ${URL}"
+        TABLEFILE="${DBDIR}/${TABLEFILE}"
         echo "TABLEFILE: ${TABLEFILE}"
 
 
-    TMPFILE=$(mktemp -t pf-tables$$) || exit 1
-    fetch -T 30 -o "${TMPFILE}" "${URL}" || exit 1
-    if [ -e "${TABLEFILE}" ]; then
-        cp ${TABLEFILE} ${TABLEFILE}.bak || exit 1
-        rm -f ${TABLEFILE} || exit 1
-    fi
-    sed -e '/^[ :space: ]*;/d' -e 's/\([0-9][/0-9.]*\) .*/\1/' ${TMPFILE} >${TABLEFILE}
+        TMPFILE=$(mktemp -t pf-tables$$) || exit 1
+        fetch -T 30 -o "${TMPFILE}" "${URL}" || exit 1
+        if [ -e "${TABLEFILE}" ]; then
+            cp ${TABLEFILE} ${TABLEFILE}.bak || exit 1
+            rm -f ${TABLEFILE} || exit 1
+        fi
+        sed -e '/^[ :space: ]*;/d' -e 's/\([0-9][/0-9.]*\) .*/\1/' ${TMPFILE} >${TABLEFILE}
 
-    rm ${TMPFILE}
+        rm ${TMPFILE}
     fi
 done < ${CONFIG}
 
