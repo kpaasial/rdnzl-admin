977fc563c929f843801e376a2e02e5c917be5529
Kimmo Paasiala <kpaasial@gmail.com>
2013-04-19 16:56:41 +0300

    Modified to allow more customization. Beat is now the default backup host.

--- backup/system-dump.sh
+++ backup/system-dump.sh
@@ -1,5 +1,10 @@
 #!/bin/sh --
 
+
+BACKUPHOST=beat.rdnzl.info
+
+BACKUPPATH=/Volumes/Backup
+
 TIMESTAMP=$( /bin/date "+%Y-%m-%d_%R:00" )
 
-/sbin/dump -C16 -b64 -0uanL -h0 -f - /    | /usr/bin/gzip -2 | /usr/bin/ssh -c blowfish kimmo@whitezone dd of=/backup/firewall/root.dump.${TIMESTAMP}.gz
+/sbin/dump -C16 -b64 -0uanL -h0 -f - /    | /usr/bin/gzip -2 | /usr/bin/ssh -c blowfish kimmo@${BACKUPHOST} dd of=${BACKUPPATH}/${HOST}-root.dump.${TIMESTAMP}.gz
