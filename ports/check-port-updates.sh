cbbd43335a77477a867bd887c79475fdcc6382d2
Kimmo Paasiala <kpaasial@gmail.com>
2013-04-29 18:17:32 +0300

    Added.

---
+++ ports/check-port-updates.sh
@@ -0,0 +1,25 @@
+#!/bin/sh --
+
+# Script for checking pkg-updating(8) for installed packages
+# and for out of date packages.
+
+# Needs a port tree at /usr/ports
+
+PKGNG=/usr/local/sbin/pkg
+
+
+cat <<EOT
+
+UPDATING entries for installed packages for the last month
+---------------------------------------------------------
+EOT
+
+${PKGNG} updating -d $(/bin/date -j -v -1m +%Y%m%d) 
+
+cat <<EOT
+
+Out of date packages.
+---------------------
+EOT
+
+${PKGNG} version -vL '='
