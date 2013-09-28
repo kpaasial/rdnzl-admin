d94e17080bf4a652f2365d8758b333f00358418d
Kimmo Paasiala <kpaasial@gmail.com>
2013-09-28 15:05:43 +0300

    Back to dump(8) on a locally mounted disk.

--- backup/system-dump.sh
+++ backup/system-dump.sh
@@ -1,10 +1,8 @@
 #!/bin/sh --
 
 
-BACKUPHOST=beat.rdnzl.info
-
-BACKUPPATH=/Volumes/Backup
+BACKUPPATH=/mnt
 
 TIMESTAMP=$( /bin/date "+%Y-%m-%d_%R:00" )
 
-/sbin/dump -C16 -b64 -0uanL -h0 -f - /    | /usr/bin/gzip -2 | /usr/bin/ssh -c blowfish kimmo@${BACKUPHOST} dd of=${BACKUPPATH}/${HOST}-root.dump.${TIMESTAMP}.gz
+/sbin/dump -C16 -b64 -0uanL -h0 -f - / | /usr/bin/gzip -2 > ${BACKUPPATH}/root.dump.${TIMESTAMP}.gz
