025e9866632be0a503c561314027f5de327953c1
Kimmo Paasiala <kpaasial@gmail.com>
2013-10-22 18:03:47 +0300

    Add hostname to the name of the dump file.

--- backup/system-dump.sh
+++ backup/system-dump.sh
@@ -1,8 +1,10 @@
 #!/bin/sh --
 
 
+: ${HOST:=$(hostname)}
+
 BACKUPPATH=/mnt
 
 TIMESTAMP=$( /bin/date "+%Y-%m-%d_%R:00" )
 
-/sbin/dump -C16 -b64 -0uanL -h0 -f - / | /usr/bin/gzip -2 > ${BACKUPPATH}/root.dump.${TIMESTAMP}.gz
+/sbin/dump -C16 -b64 -0uanL -h0 -f - / | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-root.dump.${TIMESTAMP}.gz
