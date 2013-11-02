049617e6c60b72a5d2649469f85be03b9daf76b0
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-02 12:02:30 +0200

    Now builds an INDEX file.

--- ports/update-ports.sh
+++ ports/update-ports.sh
@@ -4,6 +4,7 @@
 
 POUDRIERE=/usr/local/bin/poudriere
 
+: ${PORTS_TREE:="default"}
 
 while getopts c o
 do
@@ -18,15 +19,22 @@ if [ -n "${CRONMODE}" ]; then
     sleep `jot -r 1 0 1200`
 fi
 
+
+
+PORTS_TREE_PATH=`${POUDRIERE} ports -lq -p ${PORTS_TREE} | (read name method path; echo $path)`
+
+
 cat <<EOT
 
-Updating the ports tree "default"
----------------------------------
+Updating the ports tree "${PORTS_TREE}" at "${PORTS_TREE_PATH}"
+---------------------------------------------------------------
 
 EOT
 
 
-$POUDRIERE ports -u -v
+$POUDRIERE ports -u -v -p "${PORTS_TREE}"
+
+cd ${PORTS_TREE_PATH} && make index
 
 exit 0
 
