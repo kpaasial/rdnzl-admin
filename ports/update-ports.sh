044a22cc9637fed2bafeeb4da9201c5af9d09f17
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-03 07:04:01 +0200

    Now prints timestamps at start and end.

--- ports/update-ports.sh
+++ ports/update-ports.sh
@@ -13,16 +13,15 @@ do
     esac
 done
 
+echo "$0 starting at $(/bin/date '+%d.%m.%Y %H:%M:%S')"
+
 # Delay for up to 1200 seconds before continuing in cron mode.
 
 if [ -n "${CRONMODE}" ]; then
     sleep `jot -r 1 0 1200`
 fi
 
-
-
 PORTS_TREE_PATH=`${POUDRIERE} ports -lq -p ${PORTS_TREE} | (read name method path; echo $path)`
-
 
 cat <<EOT
 
@@ -31,10 +30,11 @@ Updating the ports tree "${PORTS_TREE}" at "${PORTS_TREE_PATH}"
 
 EOT
 
-
 $POUDRIERE ports -u -v -p "${PORTS_TREE}"
 
 cd ${PORTS_TREE_PATH} && make index
+
+echo "$0 done at $(/bin/date '+%d.%m.%Y %H:%M:%S')"
 
 exit 0
 
