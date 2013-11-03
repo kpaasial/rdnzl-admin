f8877c5e5113943319f89b37e84471f1c9a2c9b5
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-03 18:33:34 +0200

    Don't pollute /var/db/pkg with local stuff.

--- packages/fetch-updating.sh
+++ packages/fetch-updating.sh
@@ -5,7 +5,13 @@ PKGNG=/usr/local/sbin/pkg
 
 FETCH=/usr/bin/fetch
 
+INSTALL=/usr/bin/install
+
 UPDATING_URL=http://freebsd10.rdnzl.info/ports/UPDATING
+
+UPDATING_DB_PATH=/var/db/rdnzl-admin
+
+
 
 cat <<EOT
 
@@ -13,7 +19,8 @@ Fetching a new UPDATING file
 ---------------------------------------------------------
 EOT
 
-${FETCH} -o /var/db/pkg/UPDATING ${UPDATING_URL}
+${INSTALL} -d -o root -g wheel ${UPDATING_DB_PATH}
+${FETCH} -o ${UPDATING_DB_PATH}/UPDATING ${UPDATING_URL}
 
 cat <<EOT
 
@@ -21,6 +28,6 @@ UPDATING entries for installed packages for the last month
 ---------------------------------------------------------
 EOT
 
-${PKGNG} updating -f /var/db/pkg/UPDATING -d $(/bin/date -j -v -1m +%Y%m%d) 
+${PKGNG} updating -f ${UPDATING_DB_PATH}/UPDATING -d $(/bin/date -j -v -1m +%Y%m%d) 
 
 exit 0
