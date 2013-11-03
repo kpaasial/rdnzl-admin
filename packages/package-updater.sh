ff2497eba8ebd0f006b2f4c8324ae74fd8a783ff
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-03 17:22:35 +0200

    Comments.

--- packages/package-updater.sh
+++ packages/package-updater.sh
@@ -3,17 +3,21 @@
 
 PKGNG=/usr/local/sbin/pkg 
 
+# Fetch updated repository catalogues
 ${PKGNG} update
 
-
+# Fetch new UPDATING file and show
+# matching entries for installed packages
 /usr/local/sbin/fetch-updating.sh
 
+# Update pkg-audit(8) database and show warnings
+# about currently installed packages
 /usr/local/sbin/fetch-audit.sh
 
+# Check for updated packages 
+${PKGNG} version -RUvL '='
 
-/usr/local/sbin/check-port-updates.sh
-
-/usr/local/sbin/check-packages.sh
+#/usr/local/sbin/check-packages.sh
 
 
 
