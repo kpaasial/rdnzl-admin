26ce7828667369de13918d0ad3e39176ceb5a600
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-03 17:21:18 +0200

    Now also shows the UPDATING entries.

--- packages/fetch-updating.sh
+++ packages/fetch-updating.sh
@@ -1,6 +1,7 @@
 #!/bin/sh --
 
 
+PKGNG=/usr/local/sbin/pkg 
 
 FETCH=/usr/bin/fetch
 
@@ -13,5 +14,13 @@ Fetching a new UPDATING file
 EOT
 
 ${FETCH} -o /var/db/pkg/UPDATING ${UPDATING_URL}
+
+cat <<EOT
+
+UPDATING entries for installed packages for the last month
+---------------------------------------------------------
+EOT
+
+${PKGNG} updating -f /var/db/pkg/UPDATING -d $(/bin/date -j -v -1m +%Y%m%d) 
 
 exit 0
