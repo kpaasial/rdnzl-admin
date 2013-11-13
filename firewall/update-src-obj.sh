ebb784c3ec5ada49c26e12f7cb0af115c54a809c
Kimmo Paasiala <kpaasial@gmail.com>
2013-11-13 12:14:45 +0200

    Don't sync the .svn directories.

--- firewall/update-src-obj.sh
+++ firewall/update-src-obj.sh
@@ -9,7 +9,7 @@ BUILD_HOST_URI=toor@freebsd10.rdnzl.info
 
 
 
-${RSYNC} -avz --delete ${BUILD_HOST_URI}:/usr/src/ /usr/src
+${RSYNC} -avz --delete --delete-excluded --exclude='.svn*' ${BUILD_HOST_URI}:/usr/src/ /usr/src
 
 ${RSYNC} -avz --delete --delete-excluded --exclude='usr/src/release*' ${BUILD_HOST_URI}:/usr/obj/ /usr/obj
 
