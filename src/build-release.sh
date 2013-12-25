844b9ddeb32e110cea2c27c25ccacb9342d7ddf9
Kimmo Paasiala <kpaasial@gmail.com>
2013-12-25 15:41:16 +0200

    Moved build-release.sh here.

--- src/build-release.sh
+++ src/build-release.sh
@@ -1,12 +1,21 @@
 #!/bin/sh --
 
-cd /usr/src || exit 1
+cd /usr/src || (echo "No /usr/src directory." && exit 1)
 
-test -f Makefile || exit 1
+test -f Makefile || (echo "System sources missing from /usr/src?" && exit 1)
 
-SVNVERSION="r$(svnliteversion)"
+: ${SVN_CMD:=$(which svn 2>/dev/null || which svnlite 2>/dev/null)}
+
+LAST_CHANGED_REV=$( ${SVN_CMD} info . | grep '^Last Changed Rev' | cut -f 2 -d':' ) 
+
+SVNVERSION=r${LAST_CHANGED_REV# }
+
+RELATIVEURL=$( ${SVN_CMD} info . | grep '^Relative URL' | cut -f 2 -d':')
+
+BRANCH="${RELATIVEURL# ^/}"
 
 echo "SVNVERSION: $SVNVERSION"
+echo "BRANCH: $BRANCH"
 
 cd /usr/src/release || exit 1
 
@@ -14,8 +23,8 @@ make clean || exit 1
 
 make -D NOPORTS -D NODVD release || exit 1
 
-make -D NOPORTS -D NODVD install DESTDIR=/var/freebsd-snapshot/stable/10/$SVNVERSION || exit 1
+make -D NOPORTS -D NODVD install DESTDIR=/var/freebsd-snapshot/${BRANCH}/${SVNVERSION} || exit 1
 
-ln -fhs /var/freebsd-snapshot/stable/10/$SVNVERSION /var/freebsd-snapshot/stable/10/latest
+ln -fhs /var/freebsd-snapshot/${BRANCH}/${SVNVERSION} /var/freebsd-snapshot/${BRANCH}/latest
 
 
