0297a18c7d02ae3f4f1d25e2072f97131efb36d1
Kimmo Paasiala <kpaasial@gmail.com>
2014-01-06 14:08:50 +0200

    Support both full svn(1) and svnlite(1). Target of the symbolic link under /var/freebsd-snapshot/branch is better as a relative to current directory link.

--- src/build-release.sh
+++ src/build-release.sh
@@ -5,12 +5,13 @@ cd /usr/src || (echo "No /usr/src directory." && exit 1)
 test -f Makefile || (echo "System sources missing from /usr/src?" && exit 1)
 
 : ${SVN_CMD:=$(which svn 2>/dev/null || which svnlite 2>/dev/null)}
+: ${SVNVERSION_CMD:=$(which svnversion 2>/dev/null || which svnliteversion 2>/dev/null)}
 
-LAST_CHANGED_REV=$( ${SVN_CMD} info . | grep '^Last Changed Rev' | cut -f 2 -d':' ) 
 
-SVNVERSION=r${LAST_CHANGED_REV# }
+SVNVERSION="r$(${SVNVERSION_CMD})"
 
-RELATIVEURL=$( ${SVN_CMD} info . | grep '^Relative URL' | cut -f 2 -d':')
+
+RELATIVEURL=$( ${SVN_CMD} info . | grep '^Relative URL:' | cut -f 2 -d':')
 
 BRANCH="${RELATIVEURL# ^/}"
 
@@ -25,6 +26,6 @@ make -D NOPORTS -D NODVD release || exit 1
 
 make -D NOPORTS -D NODVD install DESTDIR=/var/freebsd-snapshot/${BRANCH}/${SVNVERSION} || exit 1
 
-ln -fhs /var/freebsd-snapshot/${BRANCH}/${SVNVERSION} /var/freebsd-snapshot/${BRANCH}/latest
+ln -fhs ${SVNVERSION} /var/freebsd-snapshot/${BRANCH}/latest
 
 
