19a0d54fd43e559e878a31cb1c73ac2153788c0a
Kimmo Paasiala <kpaasial@gmail.com>
2014-08-12 05:22:23 +0300

    Using find(1) gives better performance than with grep(1).

--- ports/nostage.sh
+++ ports/nostage.sh
@@ -5,5 +5,5 @@ POUDRIERE="/usr/local/bin/poudriere"
 PORTS_TREE="default"
 PORTS_TREE_PATH=`${POUDRIERE} ports -lq | grep "^${PORTS_TREE}" | (read name method date time path; echo $path)`
 
-/usr/bin/grep --include="${PORTS_TREE_PATH}/*/*/Makefile" -lr "^NO_STAGE" "${PORTS_TREE_PATH}" | \
+/usr/bin/find "${PORTS_TREE_PATH}" -mindepth 3 -maxdepth 3 -type f -name Makefile -exec grep -l ^NO_STAGE {} \+ | \
     sed -e "s|^${PORTS_TREE_PATH}/\(.*/.*\)/Makefile|\1|g"  | /usr/bin/sort
