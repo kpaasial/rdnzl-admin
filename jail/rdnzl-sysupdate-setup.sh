48150cc09d4aad7a359d7f905ab8e8b6aa68a3a2
Kimmo Paasiala <kpaasial@gmail.com>
2014-09-12 07:14:02 +0300

    Moved some common settings here.

--- jail/rdnzl-sysupdate-setup.sh
+++ jail/rdnzl-sysupdate-setup.sh
@@ -1,5 +1,16 @@
 # Some common settings that don't depend on anything else
 
+# The name of the ZFS pool.
+ZFS_POOL="zrdnzltank"
+
+# Jails are located under this ZFS filesystem
+JAIL_BASEFS="${ZFS_POOL}/DATA/jails"
+
+# System sources are kept under this ZFS filesystem
+SRC_BASEFS="${ZFS_POOL}/DATA/src"
+
+
+# Basis for the ZFS user properties used.
 USERPROPBASE="info.rdnzl"
 
 KERNELREVPROP="${USERPROPBASE}:kernelsvnrevision"
