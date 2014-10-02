47b35f8b253d5826e1b245cade7a8ef609692714
Kimmo Paasiala <kpaasial@gmail.com>
2014-10-03 02:26:20 +0300

    Renamed.

--- ports/nostage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh --
-
-POUDRIERE="/usr/local/bin/poudriere"
-
-PORTS_TREE="default"
-PORTS_TREE_PATH=`${POUDRIERE} ports -lq | grep "^${PORTS_TREE}" | (read name method date time path; echo $path)`
-
-/usr/bin/find "${PORTS_TREE_PATH}" -mindepth 3 -maxdepth 3 -type f -name Makefile -exec grep -l ^NO_STAGE {} \+ | \
-    sed -e "s|^${PORTS_TREE_PATH}/\(.*/.*\)/Makefile|\1|g"  | /usr/bin/sort
