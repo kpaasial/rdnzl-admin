b2b94c1fdfe24a9b2febec18a11e68e64d5f3e6a
Kimmo Paasiala <kpaasial@gmail.com>
2015-02-25 22:50:04 +0200

    Remove the non-working experiment.

--- firewall/load-pf-tables.sh
+++ firewall/load-pf-tables.sh
@@ -13,16 +13,7 @@ do
         echo "TABLE: ${TABLE}"
 
         /sbin/pfctl -T flush -t "${TABLE}"
-
-
-        #while read PREFIX
-        #do
         /sbin/pfctl -T add -t "${TABLE}" -f "${TABLEFILE}"
-
-
-        #done < "${TABLEFILE}"
-
-
     fi
 done < "${CONFIG}"
 
