fea250db7c95ad84d8eb5eca17bfb858283afe0e
Kimmo Paasiala <kpaasial@gmail.com>
2015-03-19 02:54:55 +0200

    Added more error handling. The tables are now downloaded into a temporary directory that is cleaned up automatically on exit. Rewrote the comment stripping filter.

--- firewall/load-pf-tables.sh
+++ firewall/load-pf-tables.sh
@@ -1,19 +1,34 @@
-#!/bin/sh
+#!/bin/sh --
 
 CONFIG=/opt/etc/pf-tables.txt
 DBDIR=/var/db/pf-tables
 
 
+if [ ! -r "${CONFIG}" ]; then
+    echo "ERROR: config file ${CONFIG} not readable."
+    exit 1
+fi
+
+if [ ! -d "${DBDIR}" ]; then
+    echo "ERROR: database directory ${DBDIR} does not exist."
+    exit 1
+fi
+
 while read URL TABLEFILE TABLE
 do
-    if [ -n "${URL}" ]; then
-        echo "URL: ${URL}"
+    if [ -n "${URL}" ] && [ -n "${TABLEFILE}" ] && [ -n "${TABLE}" ]; then
         TABLEFILE="${DBDIR}/${TABLEFILE}"
         echo "TABLEFILE: ${TABLEFILE}"
         echo "TABLE: ${TABLE}"
-
-        /sbin/pfctl -T flush -t "${TABLE}"
-        /sbin/pfctl -T add -t "${TABLE}" -f "${TABLEFILE}"
+    
+        if [ -r "${TABLEFILE}" ]; then 
+            /sbin/pfctl -T flush -t "${TABLE}"
+            /sbin/pfctl -T add -t "${TABLE}" -f "${TABLEFILE}"
+        else
+            echo "ERROR: table file ${TABLEFILE} not readable."
+            exit 1
+        fi
+        
     fi
 done < "${CONFIG}"
 
--- firewall/update-pf-tables.sh
+++ firewall/update-pf-tables.sh
@@ -1,26 +1,60 @@
-#!/bin/sh
+#!/bin/sh --
 
 CONFIG=/opt/etc/pf-tables.txt
 DBDIR=/var/db/pf-tables
 
 
+if [ ! -r "${CONFIG}" ]; then
+    echo "ERROR: config file ${CONFIG} not readable."
+    exit 1
+fi
+
+
+if [ ! -d "${DBDIR}" ]; then
+    echo "ERROR: database directory ${DBDIR} does not exist."
+    exit 1
+fi
+
+TEMPLATE="XXXXXXXXXX"
+
+SCRATCH=$(/usr/bin/mktemp -d -t pftables.${TEMPLATE})
+
+finish() {
+    # Make sure we are deleting a temporary directory created by this script
+    # and not something else. This script will be run as root, better safe than
+    # sorry.  
+    case "${SCRATCH}/" in 
+        /tmp/pftables.${TEMPLATE}.*) /bin/rm -rf "${SCRATCH}";;
+        *)      echo "Unexpected value for SCRATCH: ${SCRATCH}";;
+    esac 
+}
+
+trap finish EXIT
+
+
+echo "SCRATCH: ${SCRATCH}"
+
 while read URL TABLEFILE TABLE
 do
-    if [ -n "${URL}" ]; then
+    if [ -n "${URL}" ] && [ -n "${TABLEFILE}" ] && [ -n "${TABLE}" ]; then
         echo "URL: ${URL}"
-        TABLEFILE="${DBDIR}/${TABLEFILE}"
         echo "TABLEFILE: ${TABLEFILE}"
 
+        TMPFILE="${SCRATCH}/${TABLEFILE}"
+        echo "TMPFILE: ${TMPFILE}"
+        /usr/bin/fetch -a -T30 -o "${TMPFILE}" "${URL}" || exit 1
 
-        TMPFILE=$(mktemp -t pf-tables$$) || exit 1
-        fetch -T 30 -o "${TMPFILE}" "${URL}" || exit 1
-        if [ -e "${TABLEFILE}" ]; then
-            cp ${TABLEFILE} ${TABLEFILE}.bak || exit 1
-            rm -f ${TABLEFILE} || exit 1
+        # Make a backup of the existing tablefile. This script will not restore
+        # the back up in case of errors though.
+        if [ -r "${DBDIR}/${TABLEFILE}" ]; then
+            cp "${DBDIR}/${TABLEFILE}" "${DBDIR}/${TABLEFILE}.bak" || exit 1
         fi
-        sed -e '/^[ :space: ]*;/d' -e 's/\([0-9][/0-9.]*\) .*/\1/' ${TMPFILE} >${TABLEFILE}
-
-        rm ${TMPFILE}
+        # Process the downloaded file if it exists and put the final version
+        # under DBDIR. The sed(1) magic tries to strip all # and ; comments
+        # and then remove empty lines.
+        if [ -r "${TMPFILE}" ]; then
+            sed -e 's/[;#].*$//g' -e '/^\s*$/d' "${TMPFILE}"  >"${DBDIR}/${TABLEFILE}"
+        fi
     fi
 done < ${CONFIG}
 
