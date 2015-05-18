231644548745cf738d0c7d7dc13fbc5298990ce6
Kimmo Paasiala <kpaasial@gmail.com>
2015-05-18 14:07:08 +0300

    Port origin is now the first argument on the command line.

--- ports/ports-dependencies.sh
+++ ports/ports-dependencies.sh
@@ -3,7 +3,7 @@
 
 
 usage() {
-    echo "usage: $0 [-h] [-p portsdir] -o origin" >&2
+    echo "usage: $0 [-h] [-a] [-b] [-p portsdir] [-r] origin" >&2
     exit 1
 }
 
@@ -15,13 +15,12 @@ MAKE_CMD=/usr/bin/make
 
 : ${DEPENDENCIES:="build"}
 
-while getopts "adho:p:r" o
+while getopts "adh:p:r" o
 do
     case "$o" in
     a)  DEPENDENCIES="all";;
     b)  DEPENDENCIES="build";;
     h)  usage;;
-    o)  PORT_ORIGIN=$OPTARG;;
     p)  POUDRIERE_PORTS_TREE=$OPTARG;;
     r)  DEPENDENCIES="run";;
     *)  usage;;
@@ -30,17 +29,14 @@ done
 
 shift $((OPTIND-1))
 
+PORT_ORIGIN="$1"
+
 if test -z "$PORT_ORIGIN"; then
-    "Error: port origin (-o) is a required option."
     usage
 fi
 
 # Path to the ports(7) tree used by poudriere
 PORTS_DIR_PATH=`${POUDRIERE} ports -lq | grep "^${POUDRIERE_PORTS_TREE}" | (read name method date time path; echo $path)`
-
-echo "PORTS_DIR_PATH: ${PORTS_DIR_PATH}"
-
-echo "PORT_ORIGIN: ${PORT_ORIGIN}"
 
 # Set variables in env(1) for make(1) command
 CMD_ENV="${CMD_ENV} PORTSDIR=${PORTS_DIR_PATH}"
@@ -54,9 +50,5 @@ POUDRIERE_PORT_DBDIR="/usr/local/etc/poudriere.d/options"
 
 CMD_ENV="${CMD_ENV} PORT_DBDIR=${POUDRIERE_PORT_DBDIR}"
 
-echo "CMD_ENV: ${CMD_ENV}"
-
-echo "@: $@"
-
-exec env -i ${CMD_ENV} "${MAKE_CMD}" -C "${PORT_ORIGIN_PATH}" "${DEPENDENCIES}-depends-list"
+exec env -i ${CMD_ENV} "${MAKE_CMD}" -C "${PORT_ORIGIN_PATH}" "${DEPENDENCIES}-depends-list" | /usr/bin/sort -u
 
