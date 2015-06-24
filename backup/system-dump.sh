5121efa0c57a147b18c583e604bc442621b4b353
Kimmo Paasiala <kpaasial@gmail.com>
2015-06-24 19:58:33 +0300

    Set PREFIX to /opt. Add /data to dumped filesystems.

--- backup/system-dump.sh
+++ backup/system-dump.sh
@@ -7,4 +7,7 @@ BACKUPPATH=/mnt
 
 TIMESTAMP=$( /bin/date "+%Y-%m-%d_%R:00" )
 
+
+/sbin/dump -C16 -b64 -0uanL -h0 -f - /data | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-data.dump.${TIMESTAMP}.gz
+
 /sbin/dump -C16 -b64 -0uanL -h0 -f - / | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-root.dump.${TIMESTAMP}.gz
