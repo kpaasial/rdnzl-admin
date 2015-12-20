6281ea3d8b3cf1fcabac43697a926455435bd266
Kimmo Paasiala <kpaasial@gmail.com>
2015-12-20 10:35:57 +0200

    Stricter regexp for matching the MAINTAINER lines.

--- ports/unmaintained-ports.sh
+++ ports/unmaintained-ports.sh
@@ -5,5 +5,5 @@ POUDRIERE="/usr/local/bin/poudriere"
 PORTS_TREE="default"
 PORTS_TREE_PATH=`${POUDRIERE} ports -lq | grep "^${PORTS_TREE}" | (read name method date time path; echo $path)`
 
-/usr/bin/find "${PORTS_TREE_PATH}" -mindepth 3 -maxdepth 3 -type f -name Makefile -exec grep -li ports@freebsd.org {} \+ | \
+/usr/bin/find "${PORTS_TREE_PATH}" -mindepth 3 -maxdepth 3 -type f -name Makefile -exec egrep -li '^MAINTAINER.*ports@freebsd.org' {} \+ | \
     sed -e "s|^${PORTS_TREE_PATH}/\(.*/.*\)/Makefile|\1|g"  | /usr/bin/sort
