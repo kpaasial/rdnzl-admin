391fd75aedabbe9effe894e991b4a19dc6c21014
Kimmo Paasiala <kpaasial@gmail.com>
2016-03-19 18:56:55 +0200

    Comment out /data.

--- backup/system-dump.sh
+++ backup/system-dump.sh
@@ -3,11 +3,11 @@
 
 : ${HOST:=$(hostname)}
 
-BACKUPPATH=/mnt
+BACKUPPATH=/backup
 
 TIMESTAMP=$( /bin/date "+%Y-%m-%d_%R:00" )
 
 
-/sbin/dump -C16 -b64 -0uanL -h0 -f - /data | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-data.dump.${TIMESTAMP}.gz
+#/sbin/dump -C16 -b64 -0uanL -h0 -f - /data | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-data.dump.${TIMESTAMP}.gz
 
 /sbin/dump -C16 -b64 -0uanL -h0 -f - / | /usr/bin/gzip -2 > ${BACKUPPATH}/${HOST}-root.dump.${TIMESTAMP}.gz
