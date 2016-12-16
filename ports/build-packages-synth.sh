11c6ae50566395661ac30c844cb28b07a1c313a0
Kimmo Paasiala <kpaasial@gmail.com>
2016-12-16 16:15:29 +0200

    New build script for building ports with ports-mgmt/synth.

---
+++ ports/build-packages-synth.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+usage()
+{
+    echo "$0 [-f portsfile] [-j jail] [-p portstree]"
+    exit 0
+}
+
+
+FLOCK=/usr/local/bin/flock
+
+: ${RDNZL_CONFIG:="/opt/etc/rdnzl-admin/rdnzl.conf"}
+
+: ${PORTS_TREE:="/usr/ports"}
+
+if [ -f ${RDNZL_CONFIG} ]; then
+    . ${RDNZL_CONFIG}
+else
+    echo "Configuration file ${RDNZL_CONFIG} missing."
+    exit 1
+fi
+
+
+while getopts "f:hj:p:" o
+do
+    case "$o" in
+    f)  PORTS_TXT="$OPTARG";;
+    h)  usage;;
+    j)  BUILD_JAIL="$OPTARG";;
+    p)  PORTS_TREE="$OPTARG";;
+    *)  usage;;
+    esac
+
+done
+
+shift $((OPTIND-1))
+
+
+exec 9>/var/db/rdnzl-admin/ports.lock
+
+if ! ${FLOCK} -n 9  ; then
+    echo "Ports tree locked, aborting.";
+    exit 1
+fi
+
+
+echo "Using ${PORTS_TXT} as the list for ports to build."
+echo "Using ${BUILD_JAIL} as the build jail."
+echo "Using ${PORTS_TREE} as the ports tree."
+
+${ENV} PORTSDIR="${PORTS_TREE}" ${SYNTH_PATH} just-build ${PORTS_TXT} 
+
+
